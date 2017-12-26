a85fa0cab9d237c4f9ab9631ec241f2c0a3e2981
Johnathan Kupferer <jkupfere@redhat.com>
2017-12-26 12:17:51 -0500

    Clean up

--- roles/ally_openshift_logging_customization/files/es-scripts/delete-index.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ES_URL='https://localhost:9200'
-curl='curl -s --cacert /etc/elasticsearch/secret/admin-ca --cert /etc/elasticsearch/secret/admin-cert --key /etc/elasticsearch/secret/admin-key'
-
-$curl -X DELETE $ES_URL/$1?pretty
